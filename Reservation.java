b8492c1ea6c84296b0be336780932c916d81881c
Dine256 <kjdinesh29@gmail.com>
2025-07-02 14:19:17 +0530

    Added window seats option

---
+++ Reservation.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+
+
+public class Reservation {
+    public static void main(String[] args) {
+        int age=10;
+        // int male;
+        // int female;
+        // int gender=1;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gender:");
+        int gender = sc.nextInt();
+//         if(gender ==0){
+//             male=1;
+//         }
+//         else{
+//             female=1;
+//         }
+        
+
+//         int []bus =new int[54];
+
+//         for(int i =0;i<=bus.length;i++){
+//              if(age<=1 && age<=10){
+//                 if()
+//             }
+            
+//         }
+       
+     }
+    
+}
